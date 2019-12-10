5b0fd0fe29195c7dc6a0d55cdf392e16ed752ad5
Mario Antolovic <marioantolovic0@gmail.com>
2019-12-10 17:39:13 +0100

    fragments and sign in

--- app/src/main/java/com/mario_antolovic/wingchat/MainActivity.kt
+++ app/src/main/java/com/mario_antolovic/wingchat/MainActivity.kt
@@ -7,11 +7,14 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.mario_antolovic.wingchat.fragments.MessagesFragment
+import com.mario_antolovic.wingchat.fragments.MyAccountFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
--- app/src/main/java/com/mario_antolovic/wingchat/fragments/MessagesFragment.kt
+++ app/src/main/java/com/mario_antolovic/wingchat/fragments/MessagesFragment.kt
@@ -1,4 +1,4 @@
-package com.mario_antolovic.wingchat.ui.dashboard
+package com.mario_antolovic.wingchat.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.mario_antolovic.wingchat.R
 
-class DashboardFragment : Fragment() {
+class MessagesFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
+    private lateinit var messagesViewModel: MessagesViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProviders.of(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        messagesViewModel =
+            ViewModelProviders.of(this).get(MessagesViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_messages, container, false)
         val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(this, Observer {
+        messagesViewModel.text.observe(this, Observer {
             textView.text = it
         })
         return root
